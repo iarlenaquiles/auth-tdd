88460f75837a052a07300a4919dd53be6823c790
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-01-31 22:45:44 -0300

    criando a config do database

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: "127.0.0.1",
+  username: "",
+  password: "",
+  database: "authtdd",
+  dialect: "postgres",
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
