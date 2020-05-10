8dfbce97eb4c0c59c4b53d8b847e7c804f39a584
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-05-09 21:40:27 -0300

    add env config

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,9 @@
 module.exports = {
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || "postgres",
   operatorsAliases: false,
   logging: false,
   define: {
