5ff82d10250cf9d09db10eeaa4f4973c5eb4dd79
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-05-09 21:20:32 -0300

    add db config

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,4 @@
 module.exports = {
-  host: "127.0.0.1",
-  username: "",
-  password: "",
-  database: "authtdd",
-  dialect: "postgres",
   operatorsAliases: false,
   logging: false,
   define: {
